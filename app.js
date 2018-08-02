2a34da1565110d033bf79ba0d71cb4e437ddb029
Amir Hosseinzadeh <amir.hosseinzadeh@c2ro.com>
2018-08-02 16:27:44 -0400

    DEV-1000 code improvement

--- app.js
+++ app.js
@@ -1,37 +1,37 @@
-const express = require('express');
-const logger = require('morgan');
-const path = require('path');
-const http = require('http');
-const bodyParser = require('body-parser');
-const config = require('./config/config');
+const express = require("express");
+const logger = require("morgan");
+const path = require("path");
+const http = require("http");
+const bodyParser = require("body-parser");
+const config = require("./config/config");
 const app = express();
 
-const authRoutes = require('./src/routes/auth');
+const authRoutes = require("./src/routes/auth");
 
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "jade");
 
-app.use(logger('dev'));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(logger("dev"));
+app.use(express.static(path.join(__dirname, "public")));
 app.use(bodyParser.json());
 
 
-app.use('/', authRoutes);
+app.use("/", authRoutes);
 
-app.set('port', config.httpServer.port);
+app.set("port", config.httpServer.port);
 
 
 const server = http.createServer(app);
-server.on('listening', onListening);
+server.on("listening", onListening);
 server.listen(config.httpServer.port);
 
 
 function onListening() {
-    console.log(config.appName + ' is listening on ' + config.httpServer.port);
+    console.log(config.appName + " is listening on " + config.httpServer.port);
 }
 
-process.on('SIGINT', function () {
-    console.log(config.appName + ' stopped. ');
+process.on("SIGINT", function () {
+    console.log(config.appName + " stopped. ");
     process.exit();
 });
 
--- src/routes/auth.js
+++ src/routes/auth.js
@@ -4,7 +4,11 @@ const express = require('express');
 const router = express.Router();
 const Authentication = require('../auth');
 
-router.post('/auth', function (request, response) {
+router.get("/auth", (request, response) => {
+    return response.json({ok: true});
+});
+
+router.post("/auth", (request, response) => {
     const body = request.body;
     if (body && body.email && body.password) {
         Authentication.authenticate(body.email, body.password).then(authenticationResponse => {
